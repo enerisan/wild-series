92871ecb752774c7899d323f8842c3bdb2cdd0c0
Irene Sánchez <enerisan@gmail.com>
2024-05-23 19:27:16 +0200

    feat: creating router programs

--- server/app/routers/api/programs/router.js
+++ server/app/routers/api/programs/router.js
@@ -6,10 +6,10 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-// Import item-related actions
+// Import program-related actions
 const { browse } = require("../../../controllers/programActions");
 
-// Route to get a list of items
+// Route to get a list of programs
 router.get("/", browse);
 
 /* ************************************************************************* */
