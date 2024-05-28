cd612b2a069de049fca8dfa00b3c5d9e6834cbba
Irene Sánchez <enerisan@gmail.com>
2024-05-28 15:19:23 +0200

    feat creating Program repository

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,4 +1,5 @@
 // Some data to make the trick
+const tables = require("../../database/tables");
 
 const categories = [
   {
@@ -12,17 +13,11 @@ const categories = [
 ];
 
 // Declare the actions
-const browse = (req, res) => {
-  if (req.query.q != null) {
-    const filteredCategories = categories.filter((category) =>
-      category.name.includes(req.query.q)
-    );
-
-    res.json(filteredCategories);
-  } else {
-    res.json(categories);
-  }
-};
+const browse = async (req, res) => {
+  const categoriesFromDB = await tables.category.readAll();
+  
+   res.json(categoriesFromDB);
+  };
 
 const read = (req, res) => {
   const parsedId = parseInt(req.params.id, 10);
--- server/database/schema.sql
+++ server/database/schema.sql
@@ -20,8 +20,9 @@ create table program (
   id int unsigned primary key auto_increment not null,
   title varchar(255) not null,
 synopsis text not null,
-  poster varchar(255),
-  year int unsigned,
+  poster varchar(255) not null,
+  country varchar(255) not null,
+  year int unsigned not null,
   category_id int unsigned not null,
   foreign key(category_id) references category(id)
 );
